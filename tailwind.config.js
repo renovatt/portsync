a7d62674d4b431d402c442206f80ff833ad220c7
Will <wlymes@gmail.com>
2023-07-08 18:37:59 -0300

    style: style details

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,33 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        backgroundPrimary: 'var(--background-primary)',
+        backgroundSecondary: 'var(--background-secondary)',
+        backgroundShadow: 'var(--background-shadow)',
+        backgroundModalShadow: 'var(--background-modal-shadow)',
+      },
+      colors: {
+        textPrimary: 'var(--text-primary)',
+      },
+      borderColor: {
+        borderPrimary: 'var(--border-primary)',
+      },
+      keyframes: {
+        fade: {
+          'from': {
+            opacity: '0',
+            transform: 'translateX(-20px)'
+          },
+          'to': {
+            opacity: '1',
+            transform: 'translateX(0px)'
+          }
+        }
+      },
+      animation: {
+        fade: 'fade .7s forwards',
+      }
     },
   },
   plugins: [],
