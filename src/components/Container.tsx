068e62d7ef3cf54949857ef2e4a734449c8f0bb0
Will <wlymes@gmail.com>
2023-07-12 17:23:38 -0300

    feat: type renamed

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,6 @@
-import { ContainerTypeProps } from '@/@types'
+import { Children } from '@/@types'
 
-export const Container = ({ children }: ContainerTypeProps) => {
+export const Container = ({ children }: Children) => {
     return (
         <main className='flex flex-1 items-center justify-center flex-col min-h-[90vh]'>
             {children}
