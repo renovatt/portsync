ae387eaa4a3b06b891d9a59b24425df864fd229b
Will <wlymes@gmail.com>
2023-07-12 17:22:14 -0300

    feat: aside component created

---
+++ src/components/Aside.tsx
@@ -0,0 +1,40 @@
+import { useGlobalContext } from '@/hooks/useContext';
+import Button from './Button';
+import { BiAddToQueue } from 'react-icons/bi';
+
+function Aside() {
+    const {
+        handleCreateProject,
+        handleCreateSkill,
+        handleCreateSoftskill
+    } = useGlobalContext();
+
+    return (
+        <section className='flex items-center justify-center absolute top-12 right-0 bg-transparent overflow-y-auto animate-fade'>
+            <section className='relative flex items-center justify-between max-w-6xl rounded-lg flex-col bg-transparent py-4'>
+                <aside className='flex flex-col'>
+                    <Button
+                        title='Projeto'
+                        width='w-44 md:w-60'
+                        svg={<BiAddToQueue />}
+                        onClick={handleCreateProject}
+                    />
+                    <Button
+                        title='Habilidade'
+                        width='w-44 md:w-60'
+                        svg={<BiAddToQueue />}
+                        onClick={handleCreateSkill}
+                    />
+                    <Button
+                        title='Competência'
+                        width='w-44 md:w-60'
+                        svg={<BiAddToQueue />}
+                        onClick={handleCreateSoftskill}
+                    />
+                </aside>
+            </section>
+        </section>
+    );
+}
+
+export default Aside;
