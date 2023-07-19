ed3ff8655b401857c27ee7fbae0c08990d687d9b
will <wlymes@gmail.com>
2023-07-19 18:15:00 -0300

    feat: error component created

---
+++ src/components/Helper/Error.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+    return (
+        <section className='flex items-center justify-center flex-col h-10 w-full bg-backgroundPrimary z-50 overflow-x-hidden'>
+            <p className='text-xl text-white font-bold text-center'>Aconteceu algum erro.</p>
+        </section>
+    )
+}
+
+export default Error
