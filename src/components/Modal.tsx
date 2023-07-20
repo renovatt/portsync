9941f706e5ae7647dae2294e365b337720696e4b
will <wlymes@gmail.com>
2023-07-20 16:51:50 -0300

    feat: image and svg added

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,8 @@
 import { ModalProps } from "@/@types";
 import { RiCloseCircleLine } from "react-icons/ri";
+import { SkeletonImage } from "./Helper/SkeletonImage";
 
-export default function Modal({ children, closeModal, toggleModal }: ModalProps) {
+export default function Modal({ children, closeModal, toggleModal, src, alt, svg }: ModalProps) {
     const handleCloseModal = (
         event: React.MouseEvent<HTMLElement> |
             React.TouchEvent<HTMLElement>
@@ -10,6 +11,7 @@ export default function Modal({ children, closeModal, toggleModal }: ModalProps)
             toggleModal();
         }
     }
+
     return (
         <section
             className="fixed inset-0 z-50 overflow-y-auto bg-backgroundShadow backdrop-blur-sm animate-fade">
@@ -17,6 +19,24 @@ export default function Modal({ children, closeModal, toggleModal }: ModalProps)
                 <section
                     className='relative flex items-start justify-between  w-[90%] max-w-6xl rounded-lg p-4 flex-col bg-backgroundPrimary m-auto overflow-y-auto overflow-x-hidden textPrimary border border-zinc-600 md:border-none'
                 >
+                    {
+                        src && (
+                            <SkeletonImage
+                                alt={alt!}
+                                src={src!}
+                            />
+                        )}
+
+                    {
+                        svg && (
+                            <object
+                                className="absolute right-12 top-14 h-20 w-20 md:right-24 md:top-20 object-cover rounded-lg"
+                                type="image/svg+xml"
+                                data={svg}>
+                            </object>
+                        )
+                    }
+
                     <RiCloseCircleLine
                         className='text-white absolute top-4 right-4 w-6 h-6 cursor-pointer hover:text-textPrimary transition-all'
                         onClick={closeModal}
@@ -24,6 +44,6 @@ export default function Modal({ children, closeModal, toggleModal }: ModalProps)
                     {children}
                 </section>
             </section>
-        </section>
+        </section >
     )
 }
